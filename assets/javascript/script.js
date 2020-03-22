36f7b55f6a42b454ece5a638bb25cd19dede2099
Ben K <ben.knight.brazil@hotmail.com>
2020-03-21 23:45:54 -0600

    Made the first question show up and button hide. Changed styles

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,69 +1,90 @@
-// Variables
+// Variables for timer
 var time = 75;
 var countDownId = document.getElementById("countdown");
 countDownId.innerText = time
-
+// Variables for quiz
+var title = document.getElementById("title");
+var content1 = document.getElementById("content1");
+var content2 = document.getElementById("content2");
+var content3 = document.getElementById("content3");
+var content4 = document.getElementById("content4");
+var allContent = [content1, content2, content3, content4,]
 // Objects for questions
-var Question1 = [{
+var question1 = {
     question: "Commonly used data types DO NOT include:",
-    choices: {
-        a: "String",
-        b: "Booleans",
-        c: "Alerts",
-        d: "Numbers",
-    },
+    choices: [
+        "1: String",
+        "2: Booleans",
+        "3: Alerts",
+        "4: Numbers",
+    ],
     answer: "c"
-}]
-var Question2 = [{
+}
+var question2 = {
     question: "The condition in an if/else statement is enclosed within _____.",
-    choices: {
-        a: "Quotes",
-        b: "Curly Brackets",
-        c: "Parentheses",
-        d: "Square Brackets",
-    },
+    choices: [
+        "1: Quotes",
+        "2: Curly Brackets",
+        "3: Parentheses",
+        "4: Square Brackets",
+    ],
     answer: "c"
-}]
-var Question3 = [{
+}
+var question3 = {
     question: "Arrays in JavaScript can be used to store ____.",
-    choices: {
-        a: "Numbers and strings",
-        b: "Other Arrays",
-        c: "Booleans",
-        d: "All the above",
-    },
+    choices: [
+        "1: Numbers and strings",
+        "2: Other Arrays",
+        "3: Booleans",
+        "4: All the above",
+    ],
     answer: "d"
-}]
-var Question4 = [{
+}
+var question4 = {
     question: "String values must be enclosed within _____ when being assigned to variables.",
-    choices: {
-        a: "Commas",
-        b: "Curly Brackets",
-        c: "Quotes",
-        d: "Parentheses",
-    },
+    choices: [
+        "1: Commas",
+        "2: Curly Brackets",
+        "3: Quotes",
+        "4: Parentheses",
+    ],
     answer: "c"
-}]
-var Question5 = [{
+}
+var question5 = {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: {
-        a: "JavaScript",
-        b: "Terminal/bash",
-        c: "For loops",
-        d: "Console.log",
-    },
+    choices: [
+        "1: JavaScript",
+        "2: Terminal/bash",
+        "3: For loops",
+        "4: Console.log"
+    ],
     answer: "d"
-}]
+}
 
 
-// Call function and set interval for countdown
+// Call function and set interval for countdown and hides start button
 function startCountDown (){
-    setInterval(countDownFunction, 1000); 
+    setInterval(countDownFunction, 1000);
+    document.querySelector(".startQuiz").style.visibility = "hidden"; 
+    return; 
 }
 // Actual countdown function
 function countDownFunction () {
     time --;
     countDownId.innerHTML = time
 }
+// Function to display questions
+function startQuiz(){
+    document.getElementById("title").innerText=[question1.question];
+    for (i = 0; i < question1.choices.length; i++) {
+        var btn = document.createElement("BUTTON")
+        btn.innerText=question1.choices[i];
+        allContent[i].appendChild(btn);
+        console.log(btn)
+    }
+
+}
+
+
 
  
