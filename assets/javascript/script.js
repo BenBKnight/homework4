f42543809f8251d7f2fb8ccab2bcbc89a5121131
Ben K <ben.knight.brazil@hotmail.com>
2020-03-22 23:14:52 -0600

    Made most code for questions and highscores functional

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,96 +3,143 @@ var time = 75;
 var countDownId = document.getElementById("countdown");
 countDownId.innerText = time
 
-// Variables for quiz
-var title = document.getElementById("title");
-var a = document.getElementById("content1");
-var b = document.getElementById("content2");
-var c = document.getElementById("content3");
-var d = document.getElementById("content4");
-var allContent = [content1, content2, content3, content4,]
-
-//var option1 = document.getElementById("content1").addEventListener("click", answered1())
-
-//function answered1 (event) {
-  //  console.log("you did it!")
-//}
-// Objects for questions
-var question1 = {
-    question: "Commonly used data types DO NOT include:",
-    choices: [
-        "1: String",
-        "2: Booleans",
-        "3: Alerts",
-        "4: Numbers",
-    ],
-    answer: "c"
-}
-var question2 = {
-    question: "The condition in an if/else statement is enclosed within _____.",
-    choices: [
-        "1: Quotes",
-        "2: Curly Brackets",
-        "3: Parentheses",
-        "4: Square Brackets",
-    ],
-    answer: "c"
-}
-var question3 = {
-    question: "Arrays in JavaScript can be used to store ____.",
-    choices: [
-        "1: Numbers and strings",
-        "2: Other Arrays",
-        "3: Booleans",
-        "4: All the above",
-    ],
-    answer: "d"
-}
-var question4 = {
-    question: "String values must be enclosed within _____ when being assigned to variables.",
-    choices: [
-        "1: Commas",
-        "2: Curly Brackets",
-        "3: Quotes",
-        "4: Parentheses",
-    ],
-    answer: "c"
-}
-var question5 = {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: [
-        "1: JavaScript",
-        "2: Terminal/bash",
-        "3: For loops",
-        "4: Console.log"
-    ],
-    answer: "d"
-}
-
+var savedHighscores = []
 
 // Call function and set interval for countdown and hides start button
 function startCountDown (){
     setInterval(countDownFunction, 1000);
-    document.querySelector(".startQuiz").style.visibility = "hidden"; 
+    document.querySelector(".main-content").className = "invisible";
+//    document.querySelector(".main-content").parentNode.removeChild(span);
     return; 
 }
 // Actual countdown function
 function countDownFunction () {
     time --;
     countDownId.innerHTML = time
+
 }
+// Reveal Question 1
+function showQuestion (){
+    document.getElementById("questionWrapper1").className = ("")
+}
+function answer1 (){
+    time -= 10;
+    document.getElementById("questionWrapper1").className = ("invisible");
+    document.getElementById("questionWrapper2").className = ("");
+    console.log("this ran")
+}
+function answer1Right (){
+    document.getElementById("questionWrapper1").className = ("invisible");
+    document.getElementById("questionWrapper2").className = ("");
+    console.log("this ran too")
+}
+function answer2 (){
+    time -= 10;
+    document.getElementById("questionWrapper2").className = ("invisible");
+    document.getElementById("questionWrapper3").className = ("");
+    console.log("this ran")
+}
+function answer2Right (){
+    document.getElementById("questionWrapper2").className = ("invisible");
+    document.getElementById("questionWrapper3").className = ("");
+    console.log("this ran too")
+}
+function answer3 (){
+    time -= 10;
+    document.getElementById("questionWrapper3").className = ("invisible");
+    document.getElementById("questionWrapper4").className = ("");
+    console.log("this ran")
+}
+function answer3Right (){
+    document.getElementById("questionWrapper3").className = ("invisible");
+    document.getElementById("questionWrapper4").className = ("");
+    console.log("this ran too")
+}
+function answer4 (){
+    time -= 10;
+    document.getElementById("questionWrapper4").className = ("invisible");
+    document.getElementById("questionWrapper5").className = ("");
+    console.log("this ran")
+}
+function answer4Right (){
+    document.getElementById("questionWrapper4").className = ("invisible");
+    document.getElementById("questionWrapper5").className = ("");
+    console.log("this ran too")
+}
+function answer5 (){
+    time -= 10;
+    document.getElementById("questionWrapper5").className = ("invisible"); 
+    document.getElementById("allDone").className = ("");
+    document.getElementById("finalScore").innerText = time;
+    document.getElementById("finalTime").className = ("");
+    document.getElementById("finalTime").innerText = time;
+    countDownId.className = ("invisible");
+}
+function answer5Right (){
+    document.getElementById("questionWrapper5").className = ("invisible");
+    document.getElementById("allDone").className = ("");
+    document.getElementById("finalScore").innerText = time;
+    document.getElementById("finalTime").className = ("");
+    document.getElementById("finalTime").innerText = time;
+    countDownId.className = ("invisible");
+}
+function submitInitials(){
+    var initials = document.getElementById("initials").value;
+    document.getElementById("allDone").className = ("invisible");
+    document.getElementById("highscorePage").className = ("");
+    var savedHighscores = {initials, time}
+    localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+    for (i = 0; i < savedHighscores.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.innerText = JSON.parse(localStorage.getItem("savedHighscores"));
+        document.getElementById("highscoreList").appendChild(listItem);
+
+        console.log(JSON.parse(localStorage.getItem(savedHighscores)))
+    }
+}
+function viewHighscoreTag(){
+
+    document.querySelector(".main-content").className = "invisible";
+
+    document.getElementById("highscorePage").className = ("");
+
+}
+function clearScores(){
+    localStorage.clear();
+    document.getElementById("highscoreList").className = "invisible";
+    console.log("This worked")
+}
+
+
+
+localStorage.setItem("apple",99);
+
+
+
+
+
+
+
+
 
 // Function to display questions
 function startQuiz(){
-    document.getElementById("title").innerText=[question1.question];
-    for (i = 0; i < question1.choices.length; i++) {
-        var btn = document.createElement("BUTTON")
-        btn.innerText=question1.choices[i];
-        allContent[i].appendChild(btn);    
-    }
-    if ()
-
+    
 }
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
